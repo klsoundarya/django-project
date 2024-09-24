1ef731e37f7822d056a0664c536cb0902528868d
klsoundarya <k.soundarya7@gmail.com>
2024-09-24 18:42:48 +0100

    create procfile

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+nvx%t(*s)6v9mvxu1u#@wbi1%^4)()#(h-y_kx8wcnr_43o7='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
